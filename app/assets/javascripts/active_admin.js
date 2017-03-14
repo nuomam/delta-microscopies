53923534cb0ce74d89c77f2ec267f9d8e2469f6e
Mamoun Benmeradi <mamoun@benmeradi.com>
2017-03-14 10:24:41 +0100

    Add Cetegories

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,6 +1,3 @@
 //= require active_admin/base
 //= require jquery-ui
-//= require jquery-fileupload
-//= require cloudinary/jquery.cloudinary
-//= require attachinary
 //= require attachments
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ADMIN
+  get "/deltadmin", to: 'deltadmin/pages#dashboard'
+  namespace :deltadmin do
+    resources :categories
+  end
+
+
 end
